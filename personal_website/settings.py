ac724f18388c1c04f3f497ea5bb472c9805ad4a8
Milan Gupta <milagu@umich.edu>
2019-08-30 18:35:12 -0400

    edited settings

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.getcwd(), 'content', 'static/')
+STATIC_ROOT = os.path.join(os.getcwd(), 'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.getcwd(), 'content', 'media')
