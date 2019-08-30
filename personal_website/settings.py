7a0e8aa6d9d48c2321bd83bb57b3ceafa0d92e9b
Milan Gupta <milagu@umich.edu>
2019-08-30 18:11:06 -0400

    added milangupta.herokuapp.com to allowed_hosts

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4gt1kxpu9$u2*u)oubxyob6v#r2!(l%ca&)mrgu!6m9bajl1=3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milangupta.herokuapp.com']
 
 
 # Application definition
